53d49743bc9a065da6c815c66105ce8f84ea3e10
meta-bay <bayiemeseret@gmail.com>
2024-08-23 04:50:48 +0300

    making change

--- 0x08-making_change/5-payment.js
+++
@@ -1,5 +0,0 @@
-const Utils = require('./utils');
-const sendPaymentRequestToApi = (totalAmount, totalShipping) => {
-  const sum = Utils.calculateNumber('SUM', totalAmount, totalShipping);
-  console.log(`The total is: ${sum}`);
-};
