688b765245b52711e43954e296245b06b4d2d641
meta-bay <bayiemeseret@gmail.com>
2024-09-05 18:33:21 +0300

    prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -8,13 +8,13 @@ def isWinner(x, nums):
         prime = [True for i in range(n + 1)]
         p = 2
         while (p * p <= n):
-            if (prime[p] == True):
+            if (prime[p]):
                 for i in range(p * p, n + 1, p):
                     prime[i] = False
             p += 1
-        
+
         return [p for p in range(2, n + 1) if prime[p]]
-    
+
     def play_round(n):
         turn = 0
         primes = prime_number(n)
@@ -27,20 +27,22 @@ def isWinner(x, nums):
             primes = [p for p in primes if p in remaining]
             turn = 1 - turn
         return 'Ben' if turn == 0 else 'Maria'
-    
+
     if x < 1 or not nums:
         return None
-    
+
     ben_wins = 0
     maria_wins = 0
 
     for n in nums:
+        if n == 1:
+            ben_wins += 1
         winner = play_round(n)
         if winner == 'Maria':
             maria_wins += 1
         else:
             ben_wins += 1
-    
+
     if ben_wins > maria_wins:
         return 'Ben'
     elif maria_wins > ben_wins:
