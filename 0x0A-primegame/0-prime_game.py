5f8ee881563a2fe740237e0327d02a276d9e5c27
meta-bay <bayiemeseret@gmail.com>
2024-09-05 19:14:33 +0300

    prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -5,15 +5,12 @@
 def isWinner(x, nums):
     """ Prime game function """
     def prime_number(n):
-        prime = [True] * (n + 1)
-        p = 2
-        while (p * p <= n):
-            if (prime[p]):
-                for i in range(p * p, n + 1, p):
-                    prime[i] = False
-            p += 1
-
-        return [p for p in range(2, n + 1) if prime[p]]
+        primes = [True] * (n + 1)
+        for i in range(2, int(n ** 0.5) + 1):
+            if primes[i]:
+                for j in range(i * i, n + 1, i):
+                    primes[j] = False
+        return [i for i in range(2, n + 1) if primes[i]]
 
     def play_round(n):
         turn = 0
